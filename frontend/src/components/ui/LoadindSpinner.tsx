a6015c0cca5a37633db6857b707c533fe34d1dd9
HafsaR <hafsa34343@gmail.com>
2023-08-23 14:18:43 -0600

    addeed loading spinner component

--- frontend/src/components/ui/LoadindSpinner.tsx
+++ frontend/src/components/ui/LoadindSpinner.tsx
@@ -2,7 +2,20 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const LoadindSpinner = () => {
-  return <FontAwesomeIcon icon={faSpinner} />;
+  return (
+    <>
+      <div className="w-full grid p-30 place-items-center h-60">
+        <div>
+          <FontAwesomeIcon
+            icon={faSpinner}
+            className="place-self-center text-pink"
+            size="3x"
+            spin
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoadindSpinner;
