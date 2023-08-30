bf50944329a67123d1288cb38a8af79f02826046
Brijeshkumar Patel <brijeshpatelcan@gmail.com>
2023-08-30 09:47:36 -0500

    fix : paragragh and button startstep

--- frontend/src/components/StartStep.tsx
+++ frontend/src/components/StartStep.tsx
@@ -15,7 +15,7 @@ const StartStep = () => {
             key={index}
             className="rounded-full bg-pink w-56 h-56 flex items-center justify-center"
           >
-            <p className="text-xl tracking-wide p-10 font-satoshi text-white font-semibold">
+            <p className="text-xl text-center tracking-wide p-10 font-satoshi text-white font-semibold">
               {step}
             </p>
           </div>
@@ -23,7 +23,7 @@ const StartStep = () => {
       </div>
       <div className="w-full mt-20 mb-10 flex justify-center items-center">
         <LinkButton
-          className="w-40"
+          className="w-40 text-center"
           href="/create-story"
           intent="teal"
           size="large"
