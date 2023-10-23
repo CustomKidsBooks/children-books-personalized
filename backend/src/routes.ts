cd2905893d5d704d537967a9ee91a822fbf0483a
HafsaR <hafsa34343@gmail.com>
2023-10-23 17:02:37 -0600

    hotfix: remove auth checkup from API

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,7 +11,7 @@ export default function (app: Express) {
   app.get("/api/books/:bookId/pages", BookController.fetchPagesForBook);
 
   // Handling POST request to create a new book
-  app.post("/api/create_book", authMiddleware, BookController.createBook);
+  app.post("/api/create_book", BookController.createBook);
   // Handling GET request to fetch all books
   app.get("/api/books", BookController.fetchBooks);
 
