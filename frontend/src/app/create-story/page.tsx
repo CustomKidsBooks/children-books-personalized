cd5c4cbd962298e592690a9adf223b597e43544f
Brijeshkumar Patel <brijeshpatelcan@gmail.com>
2023-10-30 10:10:26 -0500

    fix story form privacy option

--- frontend/src/app/create-story/page.tsx
+++ frontend/src/app/create-story/page.tsx
@@ -2,17 +2,20 @@
 
 import StoryForm from "@components/StoryForm";
 import useCreateStory from "@components/hooks/useCreateStory";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
-function CreateStory() {
+const CreateStory = () => {
   const { isError, submitting, createStory } = useCreateStory();
+  const { user } = useUser();
 
   return (
     <StoryForm
       isError={isError}
       submitting={submitting}
       handleCreateStory={createStory}
+      userID={user?.sub}
     />
   );
-}
+};
 
 export default CreateStory;
